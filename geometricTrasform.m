158ebf6a37436374125ebab1741970881394f745
Varun Kumar Nagandla <varun@Varuns-MacBook-Air.local>
2020-01-25 13:47:48 +0530

    tested matlab code

--- geometricTrasform.m
+++ geometricTrasform.m
@@ -1,58 +1,41 @@
-boxImage = imread('stapleRemover.jpg');
-figure;
-imshow(boxImage);
-title('Image of a Box');
+boxImage = imread('data/negative.jpg');
+boxImage = imresize(boxImage,0.5);
+boxImage = rgb2gray(boxImage);
 
-
-sceneImage = imread('clutteredDesk.jpg');
-figure;
-imshow(sceneImage);
-title('Image of a Cluttered Scene');
+sceneImage = imread('data/doc.tif');
+sceneImage = imresize(sceneImage,0.5);
+sceneImage = rgb2gray(sceneImage);
 
 boxPoints = detectSURFFeatures(boxImage);
 scenePoints = detectSURFFeatures(sceneImage);
-
-% figure;
-% imshow(boxImage);
-% title('100 Strongest Feature Points from Box Image');
-% hold on;
-% plot(selectStrongest(boxPoints, 100));
-
-% figure;
-% imshow(sceneImage);
-% title('300 Strongest Feature Points from Scene Image');
-% hold on;
-% plot(selectStrongest(scenePoints, 300));
 
 [boxFeatures, boxPoints] = extractFeatures(boxImage, boxPoints);
 [sceneFeatures, scenePoints] = extractFeatures(sceneImage, scenePoints);
 
 boxPairs = matchFeatures(boxFeatures, sceneFeatures);
 
-matchedBoxPoints = boxPoints(boxPairs(:, 1), :);
-matchedScenePoints = scenePoints(boxPairs(:, 2), :);
-figure;
-showMatchedFeatures(boxImage, sceneImage, matchedBoxPoints, matchedScenePoints, 'montage');
-title('Putatively Matched Points (Including Outliers)');
+matched_points = size(boxPairs,1);
 
-
-[tform, inlierBoxPoints, inlierScenePoints] = estimateGeometricTransform(matchedBoxPoints, matchedScenePoints, 'affine');
-
-figure;
-showMatchedFeatures(boxImage, sceneImage, inlierBoxPoints, inlierScenePoints, 'montage');
-title('Matched Points (Inliers Only)');
-
-
-boxPolygon = [1, 1;...                           % top-left
-        size(boxImage, 2), 1;...                 % top-right
-        size(boxImage, 2), size(boxImage, 1);... % bottom-right
-        1, size(boxImage, 1);...                 % bottom-left
-        1, 1];                   % top-left again to close the polygon
-
-newBoxPolygon = transformPointsForward(tform, boxPolygon);
-
-figure;
-imshow(sceneImage);
-hold on;
-line(newBoxPolygon(:, 1), newBoxPolygon(:, 2), 'Color', 'y');
-title('Detected Box');
+if matched_points > 3
+    matchedBoxPoints = boxPoints(boxPairs(:, 1), :);
+    matchedScenePoints = scenePoints(boxPairs(:, 2), :);
+    
+    [tform, inlierBoxPoints, inlierScenePoints] = estimateGeometricTransform(matchedBoxPoints, matchedScenePoints, 'affine');
+    
+    boxPolygon = [1, 1;...                           % top-left
+            size(boxImage, 2), 1;...                 % top-right
+            size(boxImage, 2), size(boxImage, 1);... % bottom-right
+            1, size(boxImage, 1);...                 % bottom-left
+            1, 1];                   % top-left again to close the polygon
+    
+    newBoxPolygon = transformPointsForward(tform, boxPolygon);
+    
+    figure;
+    imshow(sceneImage);
+    hold on;
+    line(newBoxPolygon(:, 1), newBoxPolygon(:, 2), 'Color', 'y');
+    title('Detected Box');
+else
+    matched_points
+end
+inliers = size(inlierBoxPoints,1);
